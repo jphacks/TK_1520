3ca1c042b1db2385ac5305fa4bc2c526a6114490
Yuta Sakata <yusaka39@gmail.com>
2015-11-28 11:00:55 +0900

    Add webview

--- android/tk20android/mobile/src/main/java/com/tk20/tk20android/activity/MainActivity.java
+++ android/tk20android/mobile/src/main/java/com/tk20/tk20android/activity/MainActivity.java
@@ -1,10 +1,18 @@
-package com.tk20.tk20android;
+package com.tk20.tk20android.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.tk20.tk20android.R;
+
+import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +22,13 @@ public class MainActivity extends Activity {
     setContentView(R.layout.activity_main);
     WebView wv = (WebView) findViewById(R.id.webView);
     wv.getSettings().setJavaScriptEnabled(true);
+    wv.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+      }
+    });
     wv.loadUrl("http://hamanishi.orz.hm/");
   }
 }
